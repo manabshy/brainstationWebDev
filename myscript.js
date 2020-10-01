260d04f0e92d5138411cb3f935cf2da5da48d0ba
MJ B <mj@MJs-MacBook-Pro.local>
2020-10-01 13:36:34 +0100

    initial commit

---
+++ myscript.js
@@ -0,0 +1,14 @@
+
+
+// This is an alert
+alert('Script is fired !')
+confirm('Are you sure !')
+document.write('MY SCRIPT')
+
+console.log('Message !!!!');
+
+function myFunction() {
+    alert('I am clicked');
+    var x = document.getElementById("btn");
+    x.style.color = "red";
+}
